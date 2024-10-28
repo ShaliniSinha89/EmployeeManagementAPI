1932d27af4fc91d53cfdc09c6f590c1da33c0c66
ShaliniSinha89 <shalini.sinha.pks@gmail.com>
2024-10-28 15:33:15 +0530

    New changes in EmployeeMgnt
    New code added in EmployeeMgntAPI

--- obj/Debug/net6.0/EmployeeManagement.AssemblyInfo.cs
+++ obj/Debug/net6.0/EmployeeManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bb7fafc24d667aadcd3c21e1d6c6d3d5fd68ce2")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
